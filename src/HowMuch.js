c0a5d0f79bc6c7743500388cc23a583b309520e3
chryswoods <chryswoods@gmail.com>
2020-01-28 16:40:47 +0000

    Got all information and the ability to add a custom supercomputer

--- src/HowMuch.js
+++ src/HowMuch.js
@@ -13,11 +13,13 @@ function round(value, places=2){
                  + 'e-' + places).toFixed(places);
 }
 
+let custom_computer = "A custom supercomputer";
+
 class HowMuch extends React.Component {
   constructor(props){
     super(props);
 
-    this.state = {computer: "Blue Crystal v4",
+    this.state = {computer: custom_computer,
                   place: "United Kingdom",
                   pue: 1.2,
                   count: 1.0,
@@ -28,6 +30,11 @@ class HowMuch extends React.Component {
                           ["week", 24*7],
                           ["month", 24*30],
                           ["year", 24*365]],
+                  custom_computer: {"power": 1000.0,
+                                    "tflops": 1000,
+                                    "cores": 50000,
+                                    "acores": 0,
+                                   }
                  };
   }
 
@@ -83,8 +90,77 @@ class HowMuch extends React.Component {
     this.setState({"time": value});
   }
 
+  slotCustomPower(value){
+    if (value < 0.1){
+      value = 0.1;
+    }
+    else if (value > 50){
+      value = 50.0;
+    }
+
+    let c = {...this.state.custom_computer};
+
+    c.power = 1000 * value;
+
+    this.setState({custom_computer: c});
+  }
+
+  slotCustomTFLOPs(value){
+    if (value < 1){
+      value = 1;
+    }
+    else if (value > 1000000){
+      value = 1000000.0;
+    }
+
+    let c = {...this.state.custom_computer};
+
+    c.tflops = value;
+
+    this.setState({custom_computer: c});
+  }
+
+  slotCustomCores(value){
+    if (value < 1){
+      value = 1;
+    }
+    else if (value > 1000000){
+      value = 1000000.0;
+    }
+
+    let c = {...this.state.custom_computer};
+
+    c.cores = value;
+
+    this.setState({custom_computer: c});
+  }
+
+  slotCustomACores(value){
+    if (value < 1){
+      value = 1;
+    }
+    else if (value > 1000000){
+      value = 1000000.0;
+    }
+
+    let c = {...this.state.custom_computer};
+
+    c.acores = value;
+
+    this.setState({custom_computer: c});
+  }
+
   getComputer(){
-    return power[this.state.computer];
+    if (this.isCustomComputer()){
+      return this.state.custom_computer;
+    }
+    else {
+      return power[this.state.computer];
+    }
+  }
+
+  isCustomComputer(){
+    return this.state.computer === custom_computer;
   }
 
   getPlace(){
@@ -96,10 +172,13 @@ class HowMuch extends React.Component {
     return kWhYear / (365*24);
   }
 
-  calculatePower(){
+  calculatePower(scale=true){
     let computer = this.getComputer();
+    let p = computer.power;
 
-    let p = this.state.percent * computer.power / 100.0;
+    if (scale){
+      p = this.state.percent * p / 100.0;
+    }
 
     return p * this.state.pue;
   }
@@ -123,10 +202,15 @@ class HowMuch extends React.Component {
     return `${round(p,3)} ${unit}${s}`;
   }
 
-  calculateNumHomes(){
-    let num_homes = this.calculatePower() / this.getPlacePower();
+  calculateNumHomes(scale=true){
+    let num_homes = this.calculatePower(scale) / this.getPlacePower();
 
-    return `${round(num_homes, 0)} homes`;
+    if (num_homes <= 1){
+      return `${round(num_homes, 0)} home`;
+    }
+    else{
+      return `${round(num_homes, 0)} homes`;
+    }
   }
 
   calculateDomestic(){
@@ -214,7 +298,7 @@ class HowMuch extends React.Component {
     }
 
     let computer = null;
-    let computers = [];
+    let computers = [{value:custom_computer, label:custom_computer}];
     keys = Object.keys(power);
     keys.sort();
 
@@ -222,44 +306,92 @@ class HowMuch extends React.Component {
       computers.push({value:keys[i], label:keys[i]});
 
       if (this.state.computer === keys[i]){
-        computer = computers[i];
+        computer = computers[i+1];
       }
+    }
+
+    if (this.isCustomComputer()){
+      computer = {value:custom_computer, label:custom_computer};
     }
 
     let kwatt = this.calculateKW();
     let numhomes = this.calculateNumHomes();
     let domestic = this.calculateDomestic();
 
-    let power_type = "reported";
-    let estimated = "";
-    let disclaimer = "";
-    if (this.getComputer().is_calculated){
-      estimated = "(*)";
-      power_type = "estimated";
-      disclaimer = <div className={styles.disclaimer}>
-                     (*) the power consumption of this supercomputer is estimated
-                     based on its reported Rmax and the average GFLOPs/Watt
-                     calculated from reporting supercomputers that year.
-                   </div>;
+    let details = null;
+    let cores = null;
+    let acores = null;
+    let cpower = null;
+
+    if (this.isCustomComputer()){
+      let c = this.state.custom_computer;
+      cores = c.cores;
+      acores = c.acores;
+      cpower = c.power;
+
+      let unit = "megawatts";
+
+      if (c.power === 1000){
+        unit = "megawatt";
+      }
+
+      details = <div>
+                  consumes
+                  <NumericInput min={0.1} max={50.0} value={c.power/1000}
+                                step={0.5}
+                                onChange={(value)=>{this.slotCustomPower(value)}}/>&nbsp;
+                  {unit}, and runs at an Rmax speed of&nbsp;
+                  <NumericInput min={1} max={1000000} value={c.tflops}
+                                step={100}
+                                onChange={(value)=>{this.slotCustomTFLOPs(value)}}/>&nbsp;
+                  TFLOPs. This is an efficiency of {round(c.tflops/c.power, 1)} GFLOPs/Watt.
+                  It contains&nbsp;
+                  <NumericInput min={1} max={1000000} value={c.cores}
+                                step={100}
+                                onChange={(value)=>{this.slotCustomCores(value)}}/>&nbsp;
+                  cores and&nbsp;
+                  <NumericInput min={1} max={1000000} value={c.acores}
+                                step={100}
+                                onChange={(value)=>{this.slotCustomACores(value)}}/>&nbsp;
+                  accelerator cores.
+                </div>
+    }
+    else{
+      let c = this.getComputer();
+      cores = c.cores;
+      acores = c.acores;
+      cpower = c.power;
+
+      if (c.is_calculated){
+        details = <div>
+                    is estimated to consume {round(c.power/1000, 3)} megawatts (based
+                    on the average efficiency of {round(c.tflops/c.power, 1)} GFLOPs / Watt
+                    of its year, and a reported RMax speed of {c.tflops} TFLOPs.
+                  </div>
+      }
+      else{
+        details = <div>
+                    is reported to consume {c.power/1000} megawatts to achieve a reported
+                    RMax speed of {c.tflops} TFLOPs (an efficiency
+                    of {round(c.tflops/c.power,1)} GFLOPs / Watt).
+                  </div>
+      }
     }
 
     return <div className={styles.container}>
              <div>
-              Using
-              <NumericInput min={0.1} max={100.0} value={this.state.percent}
-                            step={5}
-                            onChange={(value)=>{this.slotChangePercent(value)}}/>% of&nbsp;
               <Select options={computers}
                       value={computer}
                       onChange={(item)=>{this.slotSelectComputer(item.value);}}/>&nbsp;
-              assuming a PUE of&nbsp;
+              {details}
+             </div>
+             <div>
+              Assuming a PUE of&nbsp;
               <NumericInput min={1.0} max={2.0} value={this.state.pue}
                             step={0.1}
                             onChange={(value)=>{this.slotChangePUE(value)}}/>&nbsp;
-              will burn electricity at a rate of&nbsp;
-              <span className={styles.result}>
-                {kwatt}
-              </span>.
+              it will burn electricity at a rate
+              of {round(0.001 * cpower * this.state.pue, 3)} megawatts.
              </div>
              <div>
                Compared to the average domestic electricity consumption in {the}&nbsp;
@@ -268,7 +400,7 @@ class HowMuch extends React.Component {
                        onChange={(item)=>{this.slotSelectPlace(item.value);}}/>,&nbsp;
                this is the same as powering&nbsp;
                <span className={styles.result}>
-                 {numhomes}
+                 {this.calculateNumHomes(false)}
                </span>.
              </div>
              <div>
@@ -279,22 +411,20 @@ class HowMuch extends React.Component {
                <Select options={times}
                       value={time}
                       onChange={(item)=>{this.slotChangeTime(item.value);}}/>&nbsp;
-               would power an average
+               that uses&nbsp;
+               <NumericInput min={0.1} max={100.0} value={this.state.percent}
+                            step={5}
+                            onChange={(value)=>{this.slotChangePercent(value)}}/>% of&nbsp;
+               this supercomputer (so {round(this.state.percent*cores/100.0, 0)} cores
+               and {round(this.state.percent*acores/100.0, 0)} accelerator cores,
+               consuming {kwatt} - equivalent to {this.calculateNumHomes(true)}) would power an average
                home in {the} {this.state.place} for&nbsp;
                <span className={styles.result}>{domestic}</span>.
              </div>
              <div className={styles.explainer}>
-               This is based on the {power_type} power consumption of {this.state.computer}&nbsp;
-               being {this.getComputer().power} kW{estimated},
-               and the total power consumption of the average home
+               This is based on the total power consumption of the average home
                in {the} {this.state.place} being {this.getPlace()} kWh per year.
              </div>
-             <div>
-               This computer has a Rmax speed of {this.getComputer().tflops} TFLOPs,
-               meaning its efficiency is {power_type} to
-               be {this.getComputer().tflops / this.getComputer().power} GFLOPs/watt.
-             </div>
-             {disclaimer}
              <div className={styles.sources}>
                <ul>
                  <li>Supercomputer power information taken from the&nbsp;
