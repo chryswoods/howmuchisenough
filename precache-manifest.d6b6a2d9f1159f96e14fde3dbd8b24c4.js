2c8e6e0b7ae7627ac1cbd23bd3b8a937f91de623
chryswoods <chryswoods@gmail.com>
2020-01-28 18:28:11 +0000

    Updates

---
+++ precache-manifest.d6b6a2d9f1159f96e14fde3dbd8b24c4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4921e8b66155e88ecec7aa19f5038223",
+    "url": "/howmuchisenough/index.html"
+  },
+  {
+    "revision": "2f8fc58cf8d53696ee56",
+    "url": "/howmuchisenough/static/css/2.a7daffc7.chunk.css"
+  },
+  {
+    "revision": "00c9db7a31fea5af6b16",
+    "url": "/howmuchisenough/static/css/4.5c338720.chunk.css"
+  },
+  {
+    "revision": "0f60cf43afdeba75cddf",
+    "url": "/howmuchisenough/static/css/main.7ca38348.chunk.css"
+  },
+  {
+    "revision": "2f8fc58cf8d53696ee56",
+    "url": "/howmuchisenough/static/js/2.79065739.chunk.js"
+  },
+  {
+    "revision": "3a5da4106a08e596432e5e2628e1ef8d",
+    "url": "/howmuchisenough/static/js/2.79065739.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8adb7f2fc89294d30d77",
+    "url": "/howmuchisenough/static/js/3.c3643e96.chunk.js"
+  },
+  {
+    "revision": "00c9db7a31fea5af6b16",
+    "url": "/howmuchisenough/static/js/4.506bcf27.chunk.js"
+  },
+  {
+    "revision": "0f60cf43afdeba75cddf",
+    "url": "/howmuchisenough/static/js/main.d1831d81.chunk.js"
+  },
+  {
+    "revision": "210f5f5d3c2ffe4f55ef",
+    "url": "/howmuchisenough/static/js/runtime-main.563da887.js"
+  }
+]);
